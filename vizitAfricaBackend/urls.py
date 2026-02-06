042e5383c4938ee4fbd3c1f3a1a57b91211dc88b
Don-tresor05 <dontresor922@gmail.com>
2026-02-06 11:58:47 +0200

    add drf_spectacular requirement

--- vizitAfricaBackend/urls.py
+++ vizitAfricaBackend/urls.py
@@ -29,8 +29,6 @@ urlpatterns = [
     path('api/services/', include('services.urls')),
     path('api/vendors/', include('vendors.urls')),
     path('api/locations/', include('locations.urls')),
-    path('api/vendors/', include('vendors.urls')),
-    path('api/services/', include('services.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
